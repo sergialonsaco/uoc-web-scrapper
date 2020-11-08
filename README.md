66a9170ddef83cddb65f75bbdb1d62c0e1293999
Sergi Alonso <sergi.alonsobdia@koahealth.com>
2020-11-08 20:36:51 +0100

    add scrape files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # uoc-web-scrapper
 Web scrapper application of UOC for the Data Science master. 
+
+# Chrome webdriver
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from scrape import ScrapeQuotes
+
+quotes = ScrapeQuotes()
+quotes.scrape()
+quotes.to_csv(filename="./quotes.csv")
+    
